d2c830104c206e319168003d02fcac809a56cbe5
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2024-12-24 20:51:59 +0630

    update login function

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -20,38 +20,45 @@
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                 </div>
-                                <form class="user">
+                                <form method="POST" action="{{ route('login') }}" class="user">
+                                    @csrf
                                     <div class="form-group">
-                                        <input type="email" class="form-control form-control-user"
+                                        <input value="{{ old('email') }}" name="email" type="email" class="form-control form-control-user"
                                             id="exampleInputEmail" aria-describedby="emailHelp"
                                             placeholder="Enter Email Address...">
+                                            @error('email')
+                                                <span class="text-danger">{{ $message }}</span>
+                                                
+                                            @enderror
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control form-control-user"
+                                        <input value="{{ old('password') }}" name="password" type="password" class="form-control form-control-user"
                                             id="exampleInputPassword" placeholder="Password">
+                                            @error('password')
+                                                <span class="text-danger">{{ $message }}</span>
+                                                @enderror
                                     </div>
-                                    <div class="form-group">
+                                    {{-- <div class="form-group">
                                         <div class="custom-control custom-checkbox small">
                                             <input type="checkbox" class="custom-control-input" id="customCheck">
                                             <label class="custom-control-label" for="customCheck">Remember
                                                 Me</label>
                                         </div>
-                                    </div>
-                                    <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                    </div> --}}
+                                    <button type="submit" href="index.html" class="btn btn-primary btn-user btn-block">
                                         Login
-                                    </a>
-                                </form>
+                                    </button>
                                     <hr>
-                                    <a href="{{ url('auth/google/redirect') }}" class="btn btn-google btn-user btn-block rounded-pill">
+                                    <a href="{{ url('auth/google/redirect') }}" class="btn btn-google btn-user btn-block">
                                         <i class="fab fa-google fa-fw"></i> Login with Google
                                     </a>
-                                    <a href="{{ url('auth/github/redirect') }}" class="btn btn-github btn-user btn-block rounded-pill fs-3">
+                                    <a href="{{ url('auth/github/redirect') }}" class="btn btn-github btn-user btn-block">
                                         <i class="fab fa-github fa-fw"></i> Login with Github
                                     </a>
-                          
+                                </form>
                                 <hr>
                                 <div class="text-center">
-                                    <a class="small" href="{{ route('password.request')}}">Forgot Password?</a>
+                                    <a class="small" href="forgot-password.html">Forgot Password?</a>
                                 </div>
                                 <div class="text-center">
                                     <a class="small" href="{{ route('register') }}">Create an Account!</a>
--- resources/views/authentication/register.blade.php
+++ resources/views/authentication/register.blade.php
@@ -8,40 +8,51 @@
         <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-           
+
                 <div class="col-lg-8 offset-2">
                     <div class="p-5">
                         <div class="text-center">
                             <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                         </div>
-                        <form class="user">
+                        <form action="{{ route('register') }}" method="post" class="user">
+                            @csrf
                             <div class="form-group row">
                                 <div class="col-sm-6 mb-3 mb-sm-0">
-                                    <input type="text" class="form-control form-control-user" id="exampleFirstName"
+                                    <input value="{{ old('name') }}" name="name" type="text" class="form-control form-control-user" id="exampleFirstName"
                                         placeholder="First Name">
+                                        @error('name')
+<small class="text-danger">{{ $message }}</small>
+                                        @enderror
                                 </div>
                                 <div class="col-sm-6">
-                                    <input type="text" class="form-control form-control-user" id="exampleLastName"
-                                        placeholder="Last Name">
+                                    <input value="{{ old('phone') }}" name="phone" type="text" class="form-control form-control-user" id="exampleLastName"
+                                        placeholder="09 xxxxxxxx">
+                                        @error('phone')
+                                        <small class="text-danger">{{ $message }}</small>
+                                        @enderror
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <input type="email" class="form-control form-control-user" id="exampleInputEmail"
+                            < class="form-group">
+                                <input value="{{old('email')}}" name="email" type="email" class="form-control form-control-user" id="exampleInputEmail"
                                     placeholder="Email Address">
-                            </div>
+                                    @error('email')
+                                    <small class="text-danger">{{ $message }}</small>
+
+                                    @enderror
+                            </  div>
                             <div class="form-group row">
                                 <div class="col-sm-6 mb-3 mb-sm-0">
-                                    <input type="password" class="form-control form-control-user"
+                                    <input name="password" type="password" class="form-control form-control-user"
                                         id="exampleInputPassword" placeholder="Password">
                                 </div>
                                 <div class="col-sm-6">
-                                    <input type="password" class="form-control form-control-user"
+                                    <input name="password_confirmation"  type="password" class="form-control form-control-user"
                                         id="exampleRepeatPassword" placeholder="Repeat Password">
                                 </div>
                             </div>
-                            <a href="login.html" class="btn btn-primary btn-user btn-block">
-                               Create
-                            </a>
+                            <button type="submit" class="btn btn-primary btn-user btn-block">
+                               Register
+                            </button>
                             <hr>
                             <a href="{{ url('auth/google/redirect') }}" class="btn btn-google btn-user btn-block">
                                 <i class="fab fa-google fa-fw"></i> Login with Google
